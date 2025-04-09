d51c3f3bb3205dc46d50f8a07f526ba78439c8d6
Daxin319 <lylehigg@gmail.com>
2025-04-09 12:40:26 -0600

    hell yeah finally got back to it

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,10 +1,47 @@
 package headers
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Headers map[string]string
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	fmt.Println("test")
-	fmt.Println("why is life so busy")
+	l := strings.Split(string(data), "\r\n")
+	total := len(data) - len("\r\n")
+	if l[0] == "\r\n" {
+		return total, true, nil
+	}
+	t := strings.TrimLeft(l[0], " ")
+	tf := strings.TrimRight(t, " ")
+	for i := range tf {
+		if string(tf[i]) == " " || string(tf[i]) == "" {
+			return 0, false, fmt.Errorf("Invalid header format - %s\n", string(data))
+		}
+		if i == 0 && string(tf[i]) == ":" {
+			return 0, false, fmt.Errorf("Invalid header format - %s\n", string(data))
+		}
+		if string(tf[i]) == ":" {
+			k := string(tf[:i])
+			tv := string(tf[i+1:])
+			stv := strings.Split(tv, "\r\n")
+			v := strings.TrimLeft(stv[0], " ")
+			h[k] = v
+			start := len(l[0]) + 2
+			_, _, err := h.Parse(data[start:])
+			if err != nil {
+				return 0, false, fmt.Errorf("Malformed header: %s\n", string(data[start:]))
+			}
+			break
+		}
+		if i == len(tf)-1 && string(tf[i]) != ":" {
+			return 0, false, fmt.Errorf("Invalid header format - %s\n", string(data))
+		}
+	}
+	return total, false, nil
+}
+
+func NewHeaders() Headers {
+	return make(Headers)
 }
