f90db95a74d8ed12f95ebafd8cc4afd0786cde10
Daxin319 <lylehigg@gmail.com>
2025-04-09 16:51:30 -0600

    cleaned up variable names for better readability

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -8,34 +8,34 @@ import (
 type Headers map[string]string
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	l := strings.Split(string(data), "\r\n")
+	split := strings.Split(string(data), "\r\n")
 	total := len(data) - len("\r\n")
-	if l[0] == "\r\n" {
+	if split[0] == "\r\n" {
 		return total, true, nil
 	}
-	t := strings.TrimLeft(l[0], " ")
-	tf := strings.TrimRight(t, " ")
-	for i := range tf {
-		if string(tf[i]) == " " || string(tf[i]) == "" {
+	trimLeft := strings.TrimLeft(split[0], " ")
+	trimRight := strings.TrimRight(trimLeft, " ")
+	for i := range trimRight {
+		if string(trimRight[i]) == " " || string(trimRight[i]) == "" {
 			return 0, false, fmt.Errorf("Invalid header format - %s\n", string(data))
 		}
-		if i == 0 && string(tf[i]) == ":" {
+		if i == 0 && string(trimRight[i]) == ":" {
 			return 0, false, fmt.Errorf("Invalid header format - %s\n", string(data))
 		}
-		if string(tf[i]) == ":" {
-			k := string(tf[:i])
-			tv := string(tf[i+1:])
-			stv := strings.Split(tv, "\r\n")
-			v := strings.TrimLeft(stv[0], " ")
-			h[k] = v
-			start := len(l[0]) + 2
+		if string(trimRight[i]) == ":" {
+			key := string(trimRight[:i])
+			tempValue := string(trimRight[i+1:])
+			splitTempValue := strings.Split(tempValue, "\r\n")
+			value := strings.TrimLeft(splitTempValue[0], " ")
+			h[key] = value
+			start := len(split[0]) + 2
 			_, _, err := h.Parse(data[start:])
 			if err != nil {
 				return 0, false, fmt.Errorf("Malformed header: %s\n", string(data[start:]))
 			}
 			break
 		}
-		if i == len(tf)-1 && string(tf[i]) != ":" {
+		if i == len(trimRight)-1 && string(trimRight[i]) != ":" {
 			return 0, false, fmt.Errorf("Invalid header format - %s\n", string(data))
 		}
 	}
